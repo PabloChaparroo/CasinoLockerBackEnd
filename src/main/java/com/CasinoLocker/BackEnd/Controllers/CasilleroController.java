77156d46f493270f162d9e7596313cbd959c0790
PabloChaparroo <pablobautic15@gmail.com>
2025-05-26 19:32:40 -0300

    Abm casillero listo

--- src/main/java/com/CasinoLocker/BackEnd/Controllers/CasilleroController.java
+++ src/main/java/com/CasinoLocker/BackEnd/Controllers/CasilleroController.java
@@ -1,13 +1,59 @@
 package com.CasinoLocker.BackEnd.Controllers;
 
 import com.CasinoLocker.BackEnd.Entitys.Casillero;
+import com.CasinoLocker.BackEnd.Entitys.EstadoCasilleroPercha;
 import com.CasinoLocker.BackEnd.Services.CasilleroServiceImpl;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
 
 @RestController
 @CrossOrigin(origins = "*")
 @RequestMapping(path = "api/casilleros")
 public class CasilleroController extends BaseControllerImpl<Casillero, CasilleroServiceImpl> {
+    @Autowired
+    private CasilleroServiceImpl service;
+
+    @PostMapping("create")
+    public ResponseEntity<?> save(@RequestBody Casillero casillero){
+        try {
+            casillero.setId(null);
+            return ResponseEntity.status(HttpStatus.OK).body(servicio.createCasillero(casillero));
+        } catch(Exception e){
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("{\"error\" Error\"}");
+        }
+    }
+
+    @PutMapping("/darDeBajaCasillero/{id}")
+    public ResponseEntity<?> darDeBajaCasillero(@PathVariable Long id) {
+        try {
+            Casillero resultado = service.darDeBajaCasillero(id);
+            return ResponseEntity.ok(resultado);
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().body(Map.of("error", e.getMessage()));
+        }
+    }
+
+    @PutMapping("/restaurar/{id}")
+    public ResponseEntity<?> restaurarCasillero(@PathVariable Long id) {
+        try {
+            Casillero resultado = service.restaurarCasillero(id);
+            return ResponseEntity.ok(resultado);
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().body(Map.of("error", e.getMessage()));
+        }
+    }
+
+    @PutMapping("/update/{id}")
+    public ResponseEntity<?> updateCasillero(@PathVariable Long id, @RequestBody Casillero casillero) {
+        try {
+            Casillero updatedCasillero = service.updateCasillero(id, casillero);
+            return ResponseEntity.ok(updatedCasillero);
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().body(Map.of("error", e.getMessage()));
+        }
+    }
 }
--- src/main/java/com/CasinoLocker/BackEnd/Services/CasilleroService.java
+++ src/main/java/com/CasinoLocker/BackEnd/Services/CasilleroService.java
@@ -1,6 +1,19 @@
 package com.CasinoLocker.BackEnd.Services;
 
 import com.CasinoLocker.BackEnd.Entitys.Casillero;
+import com.CasinoLocker.BackEnd.Entitys.EstadoCasilleroPercha;
+
+import java.util.List;
+
+import static org.springframework.data.jpa.domain.AbstractPersistable_.id;
 
 public interface CasilleroService extends BaseService<Casillero,Long>{
+
+    List<Casillero> getCasillerosNoBaja();
+
+    Casillero createCasillero(Casillero casillero) throws Exception;
+    Casillero updateCasillero(Long id, Casillero casillero) throws Exception;
+    Casillero darDeBajaCasillero(Long id) throws Exception;
+    Casillero restaurarCasillero(Long id) throws Exception;
+    void deleteCasillero(Long id) throws Exception;
 }
